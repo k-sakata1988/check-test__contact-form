c7b7098d9f2845d2ea25dff5f33e8b91b11a2488
koheisakata <koheisakata2022@gmail.com>
2025-10-03 00:18:57 +0900

    thanksページまで完了

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,14 +1,22 @@
-@extends('layouts.app')
-@section('css')
-<link rel="stylesheet" href="{{asset('css/thanks.css')}}">
-@endsection
-@section('content')
-    <div class="thanks__content">
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Thanks</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+</head>
+<body>
+  <div class="thanks__content">
+    <div class="thanks__inner">
       <div class="thanks__heading">
         <h2>お問い合わせありがとうございました</h2>
       </div>
       <div class="thanks__button">
-        <a href="{{ url('/contacts') }}" class="btn">HOME</a>
+        <a href="{{ url('/') }}" class="btn">HOME</a>
       </div>
     </div>
-@endsection
+  </div>
+</body>
+</html>
