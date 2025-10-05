b4baaa2bec08285fb605fbc298f83a5459490470
koheisakata <koheisakata2022@gmail.com>
2025-10-05 13:04:30 +0900

    微調整

--- src/resources/views/admin/admin.blade.php
+++ src/resources/views/admin/admin.blade.php
@@ -9,7 +9,7 @@
     <li class="header-nav__item">
         <form class="form" action="/logout" method="post">
             @csrf
-            <button class="header-nav__button">ログアウト</button>
+            <button class="header-nav__button">logout</button>
         </form>
     </li>
 </ul>
